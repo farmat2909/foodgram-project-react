901e1a0ba3b81fc2b4e82ff80bbcd980546b2d5c
Dmitry Stelmakhov <jaiz@yandex.ru>
2022-08-07 00:58:03 +0300

    workflow14

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='django-insecure-te)mh@f8)8i1%44*1r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', '51.250.26.163']
 
 
 # Application definition
