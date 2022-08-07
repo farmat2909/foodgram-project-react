7f71c972429884d16ff833b9d1f054247257bb5b
Dmitry Stelmakhov <jaiz@yandex.ru>
2022-08-08 01:32:47 +0300

    workflow40

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -34,10 +34,10 @@ class RecipeFilter(filters.FilterSet):
         field_name='tags__slug',
         to_field_name='slug',
     )
-    is_in_shopping_cart = filters.BooleanFilter(
+    is_in_shopping_cart = filters.NumberFilter(
         field_name='is_in_shopping_cart', method='filter'
     )
-    is_favorited = filters.BooleanFilter(
+    is_favorited = filters.NumberFilter(
         field_name='is_favorited', method='filter'
     )
 
