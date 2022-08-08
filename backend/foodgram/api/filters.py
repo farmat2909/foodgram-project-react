736774292c1d0b1d09ed93e764a605481ac64902
Dmitry Stelmakhov <jaiz@yandex.ru>
2022-08-08 20:45:58 +0300

    workflow41

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -34,24 +34,20 @@ class RecipeFilter(filters.FilterSet):
         field_name='tags__slug',
         to_field_name='slug',
     )
-    is_in_shopping_cart = filters.NumberFilter(
-        field_name='is_in_shopping_cart', method='filter'
-    )
-    is_favorited = filters.NumberFilter(
-        field_name='is_favorited', method='filter'
-    )
-
-    def filter(self, queryset, name, value):
-        if name == 'is_in_shopping_cart' and value:
-            queryset = queryset.filter(
-                shopping_cart__user=self.request.user
-            )
-        if name == 'is_favorited' and value:
-            queryset = queryset.filter(
-                favorites__user=self.request.user
-            )
-        return queryset
+    is_in_shopping_cart = filters.BooleanFilter(
+        method='get_is_in_shopping_cart')
+    is_favorited = filters.BooleanFilter(method='get_is_favorited')
 
     class Meta:
         model = Recipe
         fields = ['author', 'tags', 'is_in_shopping_cart', 'is_favorited']
+
+    def get_is_favorited(self, queryset, name, value):
+        if self.request.user.is_authenticated and value:
+            return queryset.filter(favorites__user=self.request.user)
+        return queryset
+
+    def get_is_in_shopping_cart(self, queryset, name, value):
+        if self.request.user.is_authenticated and value:
+            return queryset.filter(shopping_cart__user=self.request.user)
+        return queryset.all()
